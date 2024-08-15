164e8c2d0fdce37adf35f419db4436ddd4e4913d
Jocelynnn11 <110930776+Jocelynnn11@users.noreply.github.com>
2024-08-15 11:37:53 -0700

    Add files via upload

---
+++ AddTwoInts.java
@@ -0,0 +1,50 @@
+// This is a comment
+/*
+* Jocelyn Sandoval
+* 08/15/24
+* AddTwoInts.Java
+ */
+
+ import java.util.Scanner; // Import the Scanner class
+
+
+public class AddTwoInts {
+    public static void main(String[] args) {
+    System.out.println(" \n\n Welcome to the double Int Program !\n\n");
+
+      int firstNum = 0;
+      int secondNum = 0;
+      int sumOfBothNumbers = 0;
+
+      System.out.println("\n The value of sumOfBothNumbers is " + sumOfBothNumbers);
+
+    // Create a scanner object
+        Scanner myScannerObject = new Scanner(System.in);
+
+     // Ask user for the first int
+        System.out.println("\n Please enter a whole number: ");
+        firstNum = myScannerObject.nextInt();
+
+        // Unit text for our first user inout
+        System.out.println("\n The value of firstNum is: " + firstNum);
+
+
+        // Ask user for the second int
+        System.out.println("\n Please enter a second whole number: ");
+        secondNum = myScannerObject.nextInt();
+
+        // Unit text for our second user inout
+        System.out.println("\n The value of secondNum is: " + secondNum);
+
+        // Processing
+        // Add the two ints input from the user.
+        // the total of firstNum and secondNum will be stored in
+        // the variable named: sumOFBothNumbers
+
+        sumOfBothNumbers = firstNum + secondNum;
+
+        // Output the val0ue of the variable named: sumOfBothNumbers
+        System.out.println("\n The Value of sumOfBothNumbers is: " +sumOfBothNumbers);
+
+    }
+}
